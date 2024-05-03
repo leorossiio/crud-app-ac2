9ae27837540fb74cce5c42cbf1b60291382d238b
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-05-02 22:52:58 -0300

    feat: adiciona a tela de usuários com a tabela e os icons em cada botão

--- Frontend/crud-app/src/app/pages/home/components/users/users.component.ts
+++ Frontend/crud-app/src/app/pages/home/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  constructor(private router: Router) { }
+  user = [
+    { nome: 'Ana', email: 'ana@example.com', senha: 'senha123', status: 'Ativo', funcao: 'Engenheiro de FE', editing: false },
+    { nome: 'Carlos', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', funcao: 'Engenheiro de BE', editing: false },
+    { nome: 'Diego', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', funcao: 'Analista de dados', editing: false },
+    { nome: 'Leonardo', email: 'carlos@example.com', senha: 'senha123', status: 'Ativo', funcao: 'Líder Técnico', editing: false },
+    { nome: 'Maria', email: 'carlos@example.com', senha: 'senha123', status: 'Inativo', funcao: 'Engenheiro de BE', editing: false }
+  ];
 
+  botaoAlterarStatus(user: any) {
+    user.status = user.status === 'Ativo' ? 'Inativo' : 'Ativo';
+  }
+
+  editarUsuario(user: any) {
+    this.router.navigate(['/app/editUser', user]); // Supondo que o ID do usuário seja 'id'
+  }
+
+  salvarEdicao(user: any) {
+    user.editing = false;
+  }
+
+  deletarUsuario(user: any) {
+    const index = this.user.indexOf(user);
+    if (index !== -1) {
+      this.user.splice(index, 1);
+    }
+  }
+
+  incluirUsuario() {
+    this.router.navigate(['/app/addUser']);
+  }
 }
