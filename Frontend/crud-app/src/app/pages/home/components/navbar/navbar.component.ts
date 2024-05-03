526cf2ea30ff1e179ed17c1f01283bc56e7b202e
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-05-02 22:49:13 -0300

    refactor: adiciona novas funcionalidades na navbar e os componentes adicionados

--- Frontend/crud-app/src/app/pages/home/components/navbar/navbar.component.ts
+++ Frontend/crud-app/src/app/pages/home/components/navbar/navbar.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  activeLink: string = 'home';
 
+
+  ngOnInit(): void {
+    const savedLink = localStorage.getItem('activeLink');
+    if (savedLink) {
+      this.activeLink = savedLink;
+    }
+  }
+  
+  setActiveLink(link: string): void {
+    this.activeLink = link;
+    localStorage.setItem('activeLink', link);
+  }
+
+  logout(){
+    localStorage.removeItem('activeLink');
+  }
+  
 }
