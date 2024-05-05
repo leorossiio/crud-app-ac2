a7b2c7ebcce25c35d901f0e029c41019ef31ee69
Matheus <souza09.matheus@gmail.com>
2024-05-04 21:33:36 -0300

    refactor: Feita as alterações no welcome, para mostrar o total de usuarios e a quantidade de usuarios por função, tambem foi feita uma estilização inicial

--- Frontend/crud-app/src/app/pages/home/components/welcome/welcome.component.ts
+++ Frontend/crud-app/src/app/pages/home/components/welcome/welcome.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
-
+  totalUsers: number = 5; 
+  userRoles: any[] = [ 
+    { name: 'Engenheiro de FE', count: 1 },
+    { name: 'Engenheiro de BE', count: 2 },
+    { name: 'Analista de Dados', count: 1 },
+    { name: 'Líder Técnico', count: 1 }
+  ];
 }
