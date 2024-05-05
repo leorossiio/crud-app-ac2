3e345939d1858f15fb6e07e5c2782542466d6c06
Guilherme <gmoraes806@gmail.com>
2024-05-05 19:37:58 -0300

    feat: tela de "adicionar usuário" configurada corretamente.

--- Frontend/crud-app/src/app/pages/home/components/add-user/add-user.component.ts
+++ Frontend/crud-app/src/app/pages/home/components/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent {
+  usuario = {
+    nome: '',
+    email: '',
+    cargo: '',
+    senha: ''
+  };
+  constructor(private router: Router) { }
+  senhaInvalida: boolean = false;
 
+  submitForm() {
+    if (this.usuario.nome && this.usuario.email && this.usuario.cargo && this.usuario.senha && !this.senhaInvalida) {
+      // Aqui você pode enviar os dados do usuário para o servidor ou fazer o que for necessário
+      console.log('Usuário enviado:', this.usuario);
+      // Limpa o formulário após o envio
+      // this.usuario = {
+      //   nome: '',
+      //   email: '',
+      //   cargo: '',
+      //   senha: ''
+      // };
+      // Alerta de usuário cadastrado corretamente
+      alert('Usuário cadastrado corretamente!\n' + `Novo usuário adicionado:\nNome: ${this.usuario.nome}\nEmail: ${this.usuario.email}\nFunção: ${this.usuario.cargo}\nSenha: ${this.usuario.senha}`);
+      
+      this.router.navigate(['/app/users']);
+    } else {
+      alert('Por favor, preencha todos os campos corretamente.');
+    }
+  }
+
+  checkSenha() { 
+    this.senhaInvalida = this.usuario.senha.length < 6 || this.usuario.senha.length > 15;
+  }
 }
