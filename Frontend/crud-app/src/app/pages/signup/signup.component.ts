252c7917e2658f6a092e4346c1be610a8768f970
Lucas Marques <lucasses10@gmail.com>
2024-05-08 19:49:28 -0300

    ft: Permitindo o cadastro se as senhas forem iguais e estiver entre 6 e 20 caracteres

--- Frontend/crud-app/src/app/pages/signup/signup.component.ts
+++ Frontend/crud-app/src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -18,7 +18,7 @@ export class SignupComponent {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
       passwordConfirmation: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
-    });
+    }, { validators: this.passwordMatchValidator });
   }
 
   showPassword: boolean = false;
@@ -28,13 +28,30 @@ export class SignupComponent {
   }
 
   onSubmit() {
-    console.log("New user added with success!")
-    console.log(this.signupForm.value);
+    if (this.signupForm.valid) {
+      console.log("New user added with success!")
+      console.log(this.signupForm.value);
 
-    this.showConfirmationMessage = true;
+      this.showConfirmationMessage = true;
 
-    setTimeout(() => {
-      this.router.navigate(["/"]);
-    }, 3000);
+      setTimeout(() => {
+        this.router.navigate(["/"]);
+      }, 3000);
+    }
+  }
+
+  // Função para validar se os campos de senha e confirmação de senha são iguais
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('passwordConfirmation')?.value;
+
+    if (password !== confirmPassword) {
+      if (control.get('passwordConfirmation')) {
+        control.get('passwordConfirmation')?.setErrors({ passwordMismatch: true });
+      }
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
   }
 }
